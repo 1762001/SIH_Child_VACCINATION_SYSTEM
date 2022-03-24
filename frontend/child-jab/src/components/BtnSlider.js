35091ac4992646b0e66690a90a1ebbcdc3e03961
1762001 <73011343+1762001@users.noreply.github.com>
2022-03-25 00:08:52 +0530

    slider

---
+++ frontend/child-jab/src/components/BtnSlider.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Slider.css";
+import leftArrow from "./left-arrow.svg";
+import rightArrow from "./right-arrow.svg";
+
+export default function BtnSlider({ direction, moveSlide }) {
+  console.log(direction, moveSlide);
+  return (
+    <button
+      onClick={moveSlide}
+      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+    >
+      <img src={direction === "next" ? rightArrow : leftArrow} />
+    </button>
+  );
+}

258681eac0c81b617bc695b44c9567106118177a
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-03-27 00:23:32 +0530

    checking

--- frontend/child-jab/src/components/Slider.js
+++ frontend/child-jab/src/components/Slider.js
@@ -95,7 +95,7 @@ const Slider = () => {
               src={
                 "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
               }
-              className="d-block w-100"
+              className="d-block w-10"
               alt="..."
             />
           </div>

ec81226ecd54c67418da4eb430ce8c519f1cad41
ShrutiPandey16 <shruti379pandey@gmail.com>
2022-03-28 00:38:39 +0530

    yudxjkz

--- frontend/child-jab/src/Homepage-src/Home-Page/donation.js
+++ frontend/child-jab/src/Homepage-src/Home-Page/donation.js
@@ -1,21 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { useHistory } from 'react-router-dom';
-// const history = useHistory()
-// import {Link, useHistory} from "react-router-dom";
 import './Donation.css'
 
-
-// <><link rel="preconnect" href="https://fonts.gstatic.com" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" /><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet" /></>
-
-// const donation = (props) => {
-//   const [credentials, setCredentials]=useState({email: "",password:""});
-//   let history = useHistory();
-//   const onChange = (e) => {
-//     // here we are using spread property 
-//     //  In this whatever property is present will be there and the overeride or add the new one
-//     setCredentials({...credentials,[e.target.name]: e.target.value})
-// }
 
 export function Donation() { 
   return ( 
@@ -37,9 +23,6 @@ export function Donation() {
         <input className='input1' type="number" id="amount" required/>
 
         <Link to="/success" className="btn btn-primary btn1">Sign up</Link>
-
-        {/* <button type="submit" onClick={() => history.push('/about-us')}>Donate Now</button> */}
-        
       </form>
   </div> 
   ); 
